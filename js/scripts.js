1e8c9f36a09b4ca4517fffece12eb6e91ca0f04e
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-02-21 13:03:07 +0300

    add empty method

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
   $(document).ready(function() {
     $('li#js').click (function (){
     $('ul#answer').prepend ('<li>JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm</li>');
-    $('ul#answer').children('li').first().click(function() {
-      $(this).remove();
-    });
     });
     $('li#glob').click (function (){
+      $('ul#answer').empty();
       $('ul#answer').prepend ('<li>In computer programming, a global variable is a variable with global scope, meaning that it is visible (hence accessible) throughout the program, unless shadowed. The set of all global variables is known as the global environment or global state.</li>');
-      $('ul#answer').remove('li');
       });
       });
