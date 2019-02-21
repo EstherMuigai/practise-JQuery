780438c8fdce15b0917e1961f41f0026770f4bc6
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-02-21 13:04:03 +0300

    empty method

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
   $(document).ready(function() {
     $('li#js').click (function (){
+    $('ul#answer').empty();
     $('ul#answer').prepend ('<li>JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm</li>');
     });
     $('li#glob').click (function (){
