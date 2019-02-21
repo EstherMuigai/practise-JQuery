1e23dee7b04e20347d8ca24f91e9cd6c03c2a018
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-02-21 12:27:28 +0300

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
-
-  $("div#click-one").click(function(event) {
-    var whatToSay = "Hello!";
-    alert(whatToSay);
-  });
-
-  $("div#click-two").click(function(event) {
-    alert(whatToSay);
-  });
-});
+  $(document).ready(function() {
+    $('li#js').click (function (){
+    $('ul#answer').prepend ('<li>JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm</li>');
+    $('ul#answer').children('li').first().click(function() {
+      $(this).remove();
+    });
+    });
+    $('li#glob').click (function (){
+      $('ul#answer').prepend ('<li>In computer programming, a global variable is a variable with global scope, meaning that it is visible (hence accessible) throughout the program, unless shadowed. The set of all global variables is known as the global environment or global state.</li>');
+      $('ul#answer').remove('li');
+      });
+      });
